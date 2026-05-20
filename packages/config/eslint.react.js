404e51c9f8a602360e8dbdce478d77277e649b2e
zdnemz <zidanemz69@gmail.com>
2026-05-21 00:53:19 +0800

    chore(config): add Next.js ESLint plugin to react config and web devDeps

--- packages/config/eslint.react.js
+++ packages/config/eslint.react.js
@@ -1,6 +1,8 @@
-import baseConfig from './eslint.base.js';
-import reactPlugin from 'eslint-plugin-react';
-import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import nextPlugin from "eslint-plugin-next";
+import reactPlugin from "eslint-plugin-react";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
+
+import baseConfig from "./eslint.base.js";
 
 /** @type {import('typescript-eslint').Config} */
 export default [
@@ -8,16 +10,17 @@ export default [
   {
     plugins: {
       react: reactPlugin,
-      'react-hooks': reactHooksPlugin,
+      "react-hooks": reactHooksPlugin,
+      "@next/next": nextPlugin,
     },
     rules: {
-      'react/react-in-jsx-scope': 'off',
-      'react/prop-types': 'off',
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn',
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
     },
     settings: {
-      react: { version: 'detect' },
+      react: { version: "detect" },
     },
   },
 ];
